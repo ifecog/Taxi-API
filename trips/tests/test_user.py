2052af73b1c4bd2e391a952c0f23696d3c540f87
progIfe <ifeoluwasamson90@gmail.com>
2023-09-28 23:08:23 +0100

    Superuser test

--- trips/tests/test_user.py
+++ trips/tests/test_user.py
@@ -6,8 +6,8 @@ class UsersManagersTests(TestCase):
     
     def test_create_user(self):
         User = get_user_model()
-        user = User.objects.create(email='test@gmail.com', password='test')
-        self.assertEqual(user.email, 'test@gmail.com')
+        user = User.objects.create_user(email='user@gmail.com', password='password')
+        self.assertEqual(user.email, 'user@gmail.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
@@ -22,7 +22,26 @@ class UsersManagersTests(TestCase):
         with self.assertRaises(TypeError):
             User.objects.create_user(email='')
         with self.assertRaises(ValueError):
-            User.objects.create_user(email='', password='test')
+            User.objects.create_user(email='', password='password')
+            
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(email='superuser@gmail.com', password='password')
+        self.assertEqual(admin_user.email, 'superuser@gmail.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        try:
+            # username is None for the AbstractUser option
+            # username does not exist for the AbstractBaseUser option
+            self.assertIsNone(admin_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email="super@user.com", password="foo", is_superuser=False)
+        
+        
             
 
             
