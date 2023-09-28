d30f50d57b239ef9140f9c9c5ff9df2ff615b652
progIfe <ifeoluwasamson90@gmail.com>
2023-09-29 00:36:10 +0100

    User model migrations

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from phonenumber_field.modelfields import PhoneNumberField
+# from phonenumber_field.modelfields import PhoneNumberField
 
 from .managers import CustomUserManager
 
@@ -11,7 +11,7 @@ from .managers import CustomUserManager
 class User(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
-    phone_number = PhoneNumberField(_('phone number'), blank=True, null=True)
+    # phone_number = PhoneNumberField(_('phone number'), blank=True, null=True)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
