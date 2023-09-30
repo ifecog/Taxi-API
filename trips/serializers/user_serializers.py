b7617c77cf44cdadecefb5792d19074d77e1f0ed
progIfe <ifeoluwasamson90@gmail.com>
2023-09-30 14:51:57 +0100

    Signup endpoint and Confirm Password addition to User Serializer

--- trips/serializers/user_serializers.py
+++ trips/serializers/user_serializers.py
@@ -7,15 +7,16 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, Refr
 
 from users.models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
     _id = serializers.SerializerMethodField(read_only=True)
     name = serializers.SerializerMethodField(read_only=True)
     is_admin = serializers.SerializerMethodField(read_only=True)
-    confirm_password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True, style={'input_type': 'password'})
     
     class Meta:
         model = get_user_model()
-        fields = ['id', '_id', 'name', 'email', 'phone_number', 'confirm_password', 'is_admin']
+        fields = ['id', '_id', 'name', 'email', 'phone_number', 'is_admin', 'confirm_password']
         
     def get__id(self, obj):
         return obj.id
@@ -34,17 +35,6 @@ class UserSerializer(serializers.ModelSerializer):
     def get_is_admin(self, obj):
         return obj.is_staff
     
-    def validate_password(self, data):
-        password = data['password']
-        confirm_password = data['confirm_password']
-        
-        if password != confirm_password:
-            return serializers.ValidationError({
-                'confirm_password': ['Passwords do not match.']
-            })
-            
-        return data
-
 
 class UserSerializerWithToken(UserSerializer):
     token = serializers.SerializerMethodField(read_only=True)
--- trips/views/user_views.py
+++ trips/views/user_views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 from django.shortcuts import render, get_object_or_404
 
-from rest_framework import viewsets, status
+from rest_framework import viewsets, serializers, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
@@ -12,6 +12,35 @@ from ..serializers.user_serializers import UserSerializer, UserSerializerWithTok
 
 # Create your views here.
 class UserViewSet(viewsets.ViewSet):
+    
+    @action(detail=False, methods=['post'])
+    def signup(self, request):
+        serializer = UserSerializerWithToken(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        # Check if 'password' and 'confirm_password' fields are present in request data
+        if 'password' not in request.data or 'confirm_password' not in request.data:
+            return Response({'error': 'Both password and confirm_password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # Check if 'password' and 'confirm_password' match
+        if request.data['password'] != request.data['confirm_password']:
+            return Response({'error': 'Passwords do not match.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer.validated_data['first_name'] = request.data['first_name']
+        serializer.validated_data['last_name'] = request.data['last_name']
+        serializer.validated_data['email'] = request.data['email']
+        serializer.validated_data['phone_number'] = request.data['phone_number']
+        serializer.validated_data['password'] = make_password(request.data['password'])
+        
+        try:
+            serializer.save()          
+            
+        except Exception as e:
+            detail = {'detail': 'User with email already exists, please login!'}
+            return Response(detail, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
     
     @action(detail=False, methods=['get'])
     def get_users(self, request):
