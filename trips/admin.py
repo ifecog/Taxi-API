5681cf51860bf25fef7d32db042c7d93bb24348a
progIfe <ifeoluwasamson90@gmail.com>
2023-10-01 14:31:20 +0100

    Admin page cleanup

--- trips/admin.py
+++ trips/admin.py
@@ -2,20 +2,6 @@ from django.contrib import admin
 
 from .models import Trip
 
-# Register your models here.
-# class TripAdmin(admin.ModelAdmin):
-#     fields = (
-#         'id', 'pick_up_address', 'drop_off_address', 'status', 'created', 'updated',
-#     )
-#     list_display = (
-#         'id', 'pick_up_address', 'drop_off_address', 'status', 'created', 'updated',
-#     )
-#     list_filter = (
-#         'status',
-#     )
-#     readonly_fields = (
-#         'id', 'created', 'updated',
-
 class TripAdmin(admin.ModelAdmin):
     list_display = ('id', 'pickup_address', 'dropoff_address', 'status', 'created_time', 'updated_time')
     list_filter = ('status',)
