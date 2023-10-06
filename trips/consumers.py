e8ef2ecabc370e30c09cba766b4bd06327709eb4
progIfe <ifeoluwasamson90@gmail.com>
2023-10-06 10:09:02 +0100

    Test for sending and recieving messages

--- trips/consumers.py
+++ trips/consumers.py
@@ -6,5 +6,15 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         await self.accept()
         
+        
     async def disconnect(self, code):
         await super().disconnect(code)
+        
+        
+    async def receive_json(self, content, **kwargs):
+        message_type = content.get('type')
+        if message_type == 'echo.message':
+            await self.send_json({
+                'type': message_type,
+                'data': content.get('data',)
+            })
--- trips/tests/test_websocket.py
+++ trips/tests/test_websocket.py
@@ -4,6 +4,7 @@ from channels.testing import WebsocketCommunicator
 from taxi.routing import application
 
 
+
 TEST_CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels.layers.InMemoryChannelLayer',
@@ -21,4 +22,21 @@ class TestWebSocket:
         )
         connected, _ = await communicator.connect()
         assert connected is True
+        await communicator.disconnect()
+        
+        
+    async def test_can_send_and_recieve_messages(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path='/taxi/'
+        )
+        connerted, _ = await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message.',
+        }
+        await communicator.send_json_to(message)
+        response = await communicator.receive_json_from()
+        assert response == message
         await communicator.disconnect()
