6a075fd48a564000acea5391b422ed13364a4795
progIfe <ifeoluwasamson90@gmail.com>
2023-10-06 09:11:34 +0100

    Web socket connection test

--- taxi/routing.py
+++ taxi/routing.py
@@ -1,5 +1,12 @@
-from channels.routing import ProtocolTypeRouter
+from django.urls import path
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from trips.consumers import TaxiConsumer
 
 
-
-application = ProtocolTypeRouter({})
+application = ProtocolTypeRouter({
+    'websocket': URLRouter([
+        path('taxi/', TaxiConsumer.as_asgi())
+    ])
+})
---
+++ trips/consumers.py
@@ -0,0 +1,10 @@
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
+
+
+class TaxiConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        
+    async def disconnect(self, code):
+        await super().disconnect(code)
