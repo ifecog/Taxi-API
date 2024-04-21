424a90593f6fb9566d47a79abb619f733b820cd7
progIfe <ifeoluwasamson90@gmail.com>
2024-04-21 17:46:44 +0100

    Add token refresh and verify views to users auth

--- trips/urls/user_urls.py
+++ trips/urls/user_urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from trips.views.user_views import signup, get_users, LoginView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     # login view
     path('signin/', LoginView.as_view(), name='user-signin'),
     path('signup/', signup, name='user-signup'),
+    
+    # Token
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token-verify'),
+    
     path('get_users/', get_users, name='get_users'),
 ]
